57e62b7417a9699d05472877d90a6158cba7af16
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-14 01:51:29 +0200

    add/remove charts!

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,2 +1,24 @@
-console.log("hello world form custome scripts")
-var grid = GridStack.init();
+var grid = GridStack.init({
+	alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+	  navigator.userAgent
+	),
+	resizable: {
+	  handles: 'e, se, s, sw, w'
+	},
+	removable: '#trash',
+	removeTimeout: 100,
+	acceptWidgets: '.newWidget'
+  });
+
+  grid.on('added', function(e, items) { console.log(e, items) });
+  grid.on('removed', function(e, items) { console.log('removed ', items) });
+  grid.on('change', function(e, items) { console.log('change ', items) });
+
+
+  // TODO: switch jquery-ui out
+  $('.newWidget').draggable({
+	revert: 'invalid',
+	scroll: false,
+	appendTo: 'body',
+	helper: 'clone'
+  });
