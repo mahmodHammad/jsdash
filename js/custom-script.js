3704056198930bdec98fcecec313137f4656eaf6
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-14 23:18:43 +0200

    adding charts works with clicks !!!

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,11 +1,10 @@
 const sidebarcharts = document.getElementById("slide-out");
 const layout = document.getElementById("layout");
-const chartid = "cats";
 const sidebarIds = Object.keys(initCharts);
 
-layout.addEventListener("drop", (e) => {
-  console.log("XXXXXXXXXXXXXXXXXx", e);
-});
+// layout.addEventListener("drop", (e) => {
+//   console.log("XXXXXXXXXXXXXXXXXx", e);
+// });
 function displaycharts() {
   const active = sidebarIds.filter((e) => {
     return initCharts[e].active;
@@ -18,10 +17,22 @@ function displaycharts() {
 }
 displaycharts();
 
+function toggle(e) {
+  const id = e.id.slice(0, -1);
+
+  console.log(initCharts[id].layout);
+  const { x, y, w, h } = initCharts[id].layout;
+  const temp = document.createElement("div");
+  temp.innerHTML = `<div class="grid-stack-item" data-gs-x=${x} data-gs-y=${y} data-gs-width=${w} data-gs-height=${h} ><div id=${id} class="grid-stack-item-content">  another widget!</div></div>`;
+
+  grid.addWidget(temp, x, y, w, h, true);
+  allcharts[id](id);
+}
+
 function renderSideBarCharts() {
   return sidebarIds.map(
-    (c) => `<li id=${c}x class="newWidget grid-stack-item" draggable="true">
-		     <div class="card-body grid-stack-item-content">
+    (c) => `<li id=${c}x class=""  onclick="toggle(${c}x)">
+		     <div class="card-body">
 			    ${initCharts[c].label}
 		     </div>
 		  </li>`
@@ -42,21 +53,21 @@ var grid = GridStack.init({
   acceptWidgets: ".newWidget",
 });
 
-grid.on("added", function (e, items) {
-  console.log(e.target);
-  const SideId = items[0].el.id;
-  const id = SideId.slice(0, -1);
+// grid.on("added", function (e, items) {
+//   console.log(e.target);
+//   const SideId = items[0].el.id;
+//   const id = SideId.slice(0, -1);
 
-  //   initCharts[droppedId].active=true
-  //   displaycharts()
-  //   console.log("droppedId",droppedId)
-  const { x, y, w, h } = initCharts[id].layout;
-  e.target.innerHTML = `<div draggable="true" class="grid-stack-item" data-gs-x=${x} data-gs-y=${y} data-gs-width=${w} data-gs-height=${h} ><div id=${id} class="grid-stack-item-content">  another widget!</div></div>`;
+//   //   initCharts[droppedId].active=true
+//   //   displaycharts()
+//   //   console.log("droppedId",droppedId)
+//   const { x, y, w, h } = initCharts[id].layout;
+//   e.target.innerHTML +=`<div draggable="true" class="grid-stack-item" data-gs-x=${x} data-gs-y=${y} data-gs-width=${w} data-gs-height=${h} ><div id=${id} class="grid-stack-item-content">  another widget!</div></div>`;
 
-  allcharts[id](id);
+//   allcharts[id](id);
 
-  console.log(e, items);
-});
+//   console.log(e, items);
+// });
 grid.on("removed", function (e, items) {
   console.log("removed ", items);
 });
