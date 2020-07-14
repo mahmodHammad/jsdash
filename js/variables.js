540cf25a15a8f2c5227c27420c173409ef3229b9
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-14 14:29:55 +0200

    display charts , display sidebar items

---
+++ js/variables.js
@@ -0,0 +1,44 @@
+const initCharts = [
+  {
+    id: "stock",
+    label: "Human Body composition ",
+    active: false,
+    layout: { x: 1, y: 0, w: 2, h: 2 },
+    hover: false,
+  },
+  {
+    id: "realstock",
+    label: "Egypt's exports",
+    active: true,
+    layout: { x: 0, y: 0, w: 2, h: 3 },
+    hover: false,
+  },
+  {
+    id: "heat",
+    label: "RealTime stock price",
+    active: false,
+    layout: { x: 0, y: 0, w: 1, h: 1 },
+    hover: false,
+  },
+  {
+    id: "hey",
+    label: "Top 5 richest peoples",
+    active: false,
+    layout: { x: 1, y: 0, w: 2, h: 2 },
+    hover: false,
+  },
+  {
+    id: "exchanges",
+    label: "Egypt's imports",
+    active: true,
+    layout: { x: 2, y: 0, w: 2, h: 3 },
+    hover: false,
+  },
+  {
+    id: "yes",
+    label: "Highest Buildings",
+    active: false,
+    layout: { x: 2, y: 0, w: 1, h: 2 },
+    hover: false,
+  },
+];
